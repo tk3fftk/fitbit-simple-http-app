f82c6bf41d508f1a33e9af55c4df306d7d960a40
Hiroki Takatsuka <tktk.stereoman@gmail.com>
2020-02-11 19:23:34 +0900

    send data to device from companion

--- settings/index.jsx
+++ settings/index.jsx
@@ -6,26 +6,13 @@ function settingsComponent(props) {
           <Text bold align="center">
             App Settings
           </Text>
-        }
-      />
-      <Section>
+        }>
         <TextInput
           title="Add List Item"
-          label="Item Name"
+          label="My label"
           placeholder="Type something"
           action="Add Item"
-          settingKeys="input01"
-          onAutocomplete={value => {
-            const autoValues = [
-              { name: "red", value: "1" },
-              { name: "orange", value: "2" },
-              { name: "yellow", value: "3" },
-              { name: "green", value: "4" },
-              { name: "blue", value: "5" },
-              { name: "purple", value: "6" }
-            ];
-            return autoValues.filter(option => option.name.startsWith(value));
-          }}
+          settingsKey="input01"
         />
       </Section>
     </Page>
