0d7714df83162f7b7578828eaece127649309928
tk3fftk <tktk.stereoman@gmail.com>
2020-02-11 17:11:40 +0900

    add sample setting

--- settings/index.jsx
+++ settings/index.jsx
@@ -8,6 +8,25 @@ function settingsComponent(props) {
           </Text>
         }
       />
+      <Section>
+        <TextInput
+          title="Add List Item"
+          label="Item Name"
+          placeholder="Type something"
+          action="Add Item"
+          onAutocomplete={value => {
+            const autoValues = [
+              { name: "red", value: "1" },
+              { name: "orange", value: "2" },
+              { name: "yellow", value: "3" },
+              { name: "green", value: "4" },
+              { name: "blue", value: "5" },
+              { name: "purple", value: "6" }
+            ];
+            return autoValues.filter(option => option.name.startsWith(value));
+          }}
+        />
+      </Section>
     </Page>
   );
 }
